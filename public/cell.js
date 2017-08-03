6b2549619f30b6c2b9bf7cc912a3e300a79cbb86
sebtn <sdebedout@gmail.com>
2017-08-03 12:58:08 -0400

    Floofil protoType added

--- public/cell.js
+++ public/cell.js
@@ -5,14 +5,11 @@ function Cell(i, j, w) {
   this.y = j * w
   this.w = w
   this.neighborCount = 0
-  if (random(1) < 0.5) { //fill half board
-  this.bee = true
-  } else {
-    this.bee = false
-  }
+  this.bee = false
   this.revealed = false
 }
 
+/*-------------------------------------------------------*/
 /*Attaching methods to Cell prototype*/
 Cell.prototype.show = function() {
   stroke(0)
@@ -27,18 +24,24 @@ Cell.prototype.show = function() {
       fill(200)
       noStroke()
       rect(this.x, this.y, this.w, this.w)
-      textAlign(CENTER)
-      fill(0)
-      text(this.neighborCount, this.x + this.w * 0.5, this.y + this.w - 5) // minus 5px to center
+      if (this.neighborCount > 0) {
+        textAlign(CENTER)
+        fill(0)
+        text(this.neighborCount, this.x + this.w * 0.5, this.y + this.w - 5) // minus 5px to center
+      }
     }
   }
 }
 
+/*-------------------------------------------------------*/
 Cell.prototype.countBees = function() {
-  if (this.bee) { return -1 }
+  if (this.bee) {
+    this.neighborCount = total 
+    return -1
+  }
   var total = 0
-  for (xoff = -1; xoff <= 1; xoff++) {
-    for (yoff = -1; yoff <= 1; yoff++) {
+  for (var xoff = -1; xoff <= 1; xoff++) {
+    for (var yoff = -1; yoff <= 1; yoff++) {
       // xoffest and yoffser to account for things 
       // only inside the grid
       var i = this.i + xoff
@@ -54,10 +57,31 @@ Cell.prototype.countBees = function() {
   this.neighborCount = total
 }
 
+/*-------------------------------------------------------*/
 Cell.prototype.contains = function(x, y) {
   return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w)
 }
 
+/*-------------------------------------------------------*/
 Cell.prototype.reveal = function() {
   this.revealed = true
+  if (this.neighborCount == 0) {
+    this.floddFill()
+  }
+}
+
+Cell.prototype.floddFill = function () {
+  // Counting neighbors inside the grid
+  for (var xoff = -1; xoff <= 1; xoff++) {
+    for (var yoff = -1; yoff <= 1; yoff++) {
+      var i = this.i + xoff
+      var j = this.j + yoff
+      if(i > -1 && i < cols && j > -1 && j < rows) {
+        var neighbor = grid[i][j]
+        if(!neighbor.bee && !neighbor.revealed) {
+          neighbor.reveal()
+        }
+      }
+    }
+  }
 }
--- public/main.js
+++ public/main.js
@@ -1,3 +1,10 @@
+var grid
+var cols
+var rows 
+var w = 20
+var totalBees = 20
+
+/*-------------------------------------------------------*/
 function make2DArray(cols, rows) {
   var arr = Array(cols)
   for (var i = 0; i < arr.length; i++) {
@@ -6,11 +13,7 @@ function make2DArray(cols, rows) {
   return arr
 }
 
-var grid
-var cols
-var rows 
-var w = 20
-
+/*-------------------------------------------------------*/
 function setup() {
   createCanvas(201, 201)
   cols = floor(width / w)
@@ -19,9 +22,29 @@ function setup() {
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
       // initialize grid using cells
-      grid[i][j] = new Cell(i,j, w)
+      grid[i][j] = new Cell(i, j, w)
     }
   }
+
+  // options
+  var options = []
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      options.push([i, j])
+    }
+  }
+
+  //pick totalBees spots 
+  for (var n = 0; n < totalBees; n++) {
+    var index = floor(random(options.length))
+    var choice = options[index]
+    var i = choice[0]
+    var j = choice[1]
+    // splice deletes the spot from options
+    options.splice(index, 1) 
+    grid[i][j].bee = true
+  }
+
   // count neighbors
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
@@ -30,6 +53,7 @@ function setup() {
   }
 }
 
+/*-------------------------------------------------------*/
 function draw() {
   background(255)
   for (var i = 0; i < cols; i++) {
@@ -40,10 +64,11 @@ function draw() {
   }
 }
 
+/*-------------------------------------------------------*/
 function mousePressed() {
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
-      // Check every spot for mouse spot
+      // Check every spot for mouse spot reveal on click
       if ( grid[i][j].contains(mouseX, mouseY) ) {
         grid[i][j].reveal()
       }
